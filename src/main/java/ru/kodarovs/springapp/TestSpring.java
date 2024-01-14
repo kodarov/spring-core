415ecc638eebb723d7cfd0b998f77c9d0be0c190
kodarov_salavat <kodarov@gmail.com>
2024-01-14 19:28:04 +0400

    7 - Spring - scope Singletone/Prototype

--- src/main/java/ru/kodarovs/springapp/TestSpring.java
+++ src/main/java/ru/kodarovs/springapp/TestSpring.java
@@ -9,16 +9,20 @@ public class TestSpring {
         /**
          * DI через контроллер
          */
-        MusicPlayer musicPlayer = context.getBean("musicPlayerWithConstructor", MusicPlayer.class);
-        musicPlayer.playMusic();
+        MusicPlayer musicPlayer0 = context.getBean("musicPlayerWithConstructor", MusicPlayer.class);
+        musicPlayer0.playMusic();
 
         /**
          * DI через сеттер
          */
+        MusicPlayer musicPlayer1 = context.getBean("musicPlayerWithConstructor", MusicPlayer.class);
         MusicPlayer musicPlayer2 = context.getBean("musicPlayerWithSetter", MusicPlayer.class);
         musicPlayer2.playMusic();
+
         System.out.println(musicPlayer2.getName());
         System.out.println(musicPlayer2.getVolume());
+
+        System.out.println(musicPlayer0 == musicPlayer1); // True - scope Singletone
 
         context.close();
     }
