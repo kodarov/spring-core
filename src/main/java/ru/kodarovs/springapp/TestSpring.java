fca0671bf226aea4555a6b5a7b26c196b196f029
kodarov_salavat <kodarov@gmail.com>
2024-01-14 19:48:28 +0400

    8 - Spring - Bean Lifecycle - init/destroy

--- src/main/java/ru/kodarovs/springapp/TestSpring.java
+++ src/main/java/ru/kodarovs/springapp/TestSpring.java
@@ -6,20 +6,8 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context
                 = new ClassPathXmlApplicationContext("applicationContext.xml");
-        /**
-         * DI через контроллер
-         */
-        MusicPlayer musicPlayer0 = context.getBean("musicPlayerWithConstructor", MusicPlayer.class);
-        musicPlayer0.playMusic();
-        /**
-         * DI через сеттер
-         */
-        MusicPlayer musicPlayer1 = context.getBean("musicPlayerWithConstructor", MusicPlayer.class);
-        MusicPlayer musicPlayer2 = context.getBean("musicPlayerWithSetter", MusicPlayer.class);
-        musicPlayer2.playMusic();
-        System.out.println(musicPlayer2.getName());
-        System.out.println(musicPlayer2.getVolume());
-        System.out.println(musicPlayer0 == musicPlayer1); // True - scope Singletone
+        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
         context.close();
     }
 }
