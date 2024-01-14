30b876218d517ebcb9adc5070ab4cf42380d18be
kodarov_salavat <kodarov@gmail.com>
2024-01-14 21:29:14 +0400

    10 - Spring - @Qualifier - Dependency Injection

--- src/main/java/ru/kodarovs/springapp/MusicPlayer.java
+++ src/main/java/ru/kodarovs/springapp/MusicPlayer.java
@@ -1,20 +1,21 @@
 package ru.kodarovs.springapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
+    private Music music1;
+    private Music music2;
 
     @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
+    public MusicPlayer(@Qualifier("rockMusic") Music music1,@Qualifier("classicalMusic") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
 
     public String playMusic() {
-        return classicalMusic.getSong();
+        return music1.getSong() + " and " + music2.getSong();
     }
 }
